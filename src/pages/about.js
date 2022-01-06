7e5eee30062725022282957b4700b726f5384b75
Miguel Ybera <josemiguel.ybera.iics@ust.edu.ph>
2022-01-06 11:17:54 +0800

    added eager and lazy

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,9 @@ const AboutPage = () => {
   return (
     <Layout pageTitle='About Page' pageHeading='About Us Page'>
       These are the contents of about page
-      <StaticImage src="https://images.indianexpress.com/2021/08/spider-man-no-way-home-1200.jpg" alt="spiderman 2"  /*for images coming from link*//>  
+      <StaticImage src="https://images.indianexpress.com/2021/08/spider-man-no-way-home-1200.jpg" alt="spiderman 2" loading='eager' /*for images coming from link*//>  
+      <StaticImage src="https://i.insider.com/61a542951f8b8c00195ab374?width=700" alt="spiderman 3" loading='lazy' /*for images coming from link*//>  
+      
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ const HomePage = () => {
   return (
     <Layout pageTitle='Home Page' pageHeading='Home'>
   <p>Welcome to the Ybera Page</p>
-  <StaticImage src="../images/sman.jpg" alt="spiderman"  /*for images coming from image folder*//>
-  <StaticImage src="https://images.firstpost.com/wp-content/uploads/2021/09/Marvels-Spider-Man-2-1280.jpg" alt="spidrmen"  /*for images coming from link*//>  
+  <StaticImage src="../images/sman.jpg" alt="spiderman" loading='eager' /*for images coming from image folder*//>
+  <StaticImage src="https://images.firstpost.com/wp-content/uploads/2021/09/Marvels-Spider-Man-2-1280.jpg" alt="spidrmen" loading='eager' /*for images coming from link*//>  
     </Layout>
     
     
